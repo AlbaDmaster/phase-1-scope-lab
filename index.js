5381cd23a0af0b86259b3e490363d2463d784753
Al-bashir <albashirabdi482@gmail.com>
2024-01-28 09:35:44 +0300

    COmpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerName(){
+   var upperCased=customerName.toUpperCase();
+   return upperCased;
+}
+
+upperCaseCustomerName();
+
+console.log(customerName);
+var bestCustomer;
+
+function setBestCustomer(){
+     bestCustomer='not bob';
+    return bestCustomer;
+}
+
+
+
+function overwriteBestCustomer(){
+     bestCustomer='maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer='bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Kim';
+    return leastFavoriteCustomer;
+}
